a47302c48219e9f38af61a1c61d783b305a00f15
joeldiazcc <joeldiazcarissimi@gmail.com>
2022-11-10 20:05:57 +0100

    main.py call

--- act6.py
+++ act6.py
@@ -2,9 +2,4 @@
 def suma(var1,var2):
     print("La suma del valor "+str(var1)+" més el valor "+str(var2)+" serà: "+str(var1)+"+"+str(var2)+"="+str(var1+var2))
 
-# Cal tindre un arxiu main on cridi a la funció i, utilitzant input().
-num1=int(input("1r. Indica un número enter per sumar: "))
-num2=int(input("2n. Indica segón número enter per sumar: "))
 
-#Cridem la funció
-suma(num1,num2)
---
+++ main.py
@@ -0,0 +1,9 @@
+#Importem funcio
+from act6 import suma
+
+# Cal tindre un arxiu main on cridi a la funció i, utilitzant input().
+num1=int(input("1r. Indica un número enter per sumar: "))
+num2=int(input("2n. Indica segón número enter per sumar: "))
+
+#Cridem la funció
+suma(num1,num2)
