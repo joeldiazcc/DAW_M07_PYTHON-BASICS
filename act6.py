8a8e42252c7f5a0d41ef1d3aadc39dd76c4f8b27
Joel Felipe Díaz Carissimi <64970845+joeldiazcc@users.noreply.github.com>
2022-11-10 19:47:47 +0100

    Revert "conc.py & act6.py"

--- act6.py
+++ act6.py
@@ -1,6 +1,6 @@
 #Crear una funció que, passat 2 paràmetres, els sumarà entre ells.
 def suma(var1,var2):
-    print("La suma del valor "+str(var1)+" més el valor "+str(var2)+" serà :"+str(var1)+"+"+str(var2)+"="+str(var1+var2))
+    print("La suma del valor", str(var1), "més el valor", str(var2), "serà:", (var1+var2))
 
 # Cal tindre un arxiu main on cridi a la funció i, utilitzant input().
 num1=int(input("1r. Indica un número enter per sumar: "))
--- conc.py
+++ conc.py
@@ -3,10 +3,12 @@ intvar1 = 12
 intvar2 = 53
 
 #Imprimir el resultat de la suma de les dues variables (int)
+
 print(intvar1+intvar2)
+
 
 #Imprimir el resultat de la suma de les dues variables (int) dintre d'un text (string)
 
-print("El resultat de la suma entre"+str(intvar1)+" y "+str(intvar2)+" es "+str(intvar1+intvar2))
+print("El resultat de la suma entre",str(intvar1)," y",str(intvar2)," es",(intvar1+intvar2))
 
 
