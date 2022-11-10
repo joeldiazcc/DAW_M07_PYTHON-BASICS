b51b08b5505cce8447fbd161b4145f2d12bbef49
joeldiazcc <joeldiazcarissimi@gmail.com>
2022-11-10 19:33:48 +0100

    conc.py

---
+++ conc.py
@@ -0,0 +1,14 @@
+#Declarem les dues variables int per després sumar.
+intvar1 = 12
+intvar2 = 53
+
+#Imprimir el resultat de la suma de les dues variables (int)
+
+print(intvar1+intvar2)
+
+
+#Imprimir el resultat de la suma de les dues variables (int) dintre d'un text (string)
+
+print("El resultat de la suma entre",str(intvar1)," y",str(intvar2)," es",(intvar1+intvar2))
+
+
