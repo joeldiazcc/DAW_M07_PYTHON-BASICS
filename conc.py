b55018a33850843866be8858b5cb06dd0906e3a2
Ivan Petrov <ipetrov20@jaumebalmes.net>
2022-11-10 20:53:37 +0100

    Calcular numero random

---
+++ conc.py
@@ -0,0 +1,9 @@
+areas = ["cuina", 7.88, "menjador", 13.0, "terrassa", 20.34, "lavabo", 6.55, "habitació1", 7.98, "habitació2", 12, "rebedor", 4.23]
+
+print(areas[1]) #part1
+print(areas[len(areas)-1]) # part1
+
+print(areas[5])
+
+for n in areas:
+    print(n)
